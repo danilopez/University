9fd5b926f9142951fdda9e9e41b3bd76b721f9d2
Daniel Lopez <tauron32@gmail.com>
2012-02-12 22:31:38 +0100

    Problema 2 resuelto.

--- Prolog/Febrero2008.pl
+++ Prolog/Febrero2008.pl
@@ -1,1 +1,8 @@
-% Problema 2. (1.0 pto.) Define el predicado suma(+Xs, -N) que dada una lista de enteros Xs devuelve en N la suma de los elementos de Xs.
+% Problema 2. (1.0 pto.) Define el predicado suma(+Xs, -N) que dada una lista de enteros Xs devuelve en N la suma de los elementos de Xs. [Recursivo de cola]
+suma(Xs,N) :- suma_cola(Xs,0,N).
+suma_cola([],X,X).
+suma_cola([X|Xs], Ac, N) :-
+	NAc is Ac + X,
+	suma_cola(Xs,NAc,N).
+
+
