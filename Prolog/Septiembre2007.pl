a480f05cfaf980a71adbdeaf1915b390811156d6
Daniel Lopez <tauron32@gmail.com>
2012-02-13 01:36:14 +0100

    Problema 3 resuelto a la primera.

--- Prolog/Septiembre2007.pl
+++ Prolog/Septiembre2007.pl
@@ -24,3 +24,22 @@ no_esta(_,[]).
 no_esta(X,[Y|Ys]) :-
 	X \== Y,
 	no_esta(X,Ys).
+	
+% Problema 3. (2.25 ptos.) Aplicando generar/comprobar, define separa(+Ls,+N,-Xs,-Ys,-Zs) que dada la lista Ls la divide en tres sublistas Xs, Ys y Zs, cumpliendo las siguientes condiciones:
+%	concatenadas las tres forman Ls
+%	Ys y Zs son listas no vacías
+% 	los elementos de la cola de Ys suman N
+% Por ejemplo:
+%
+% ?- separa([1,2,1,3,2,1],3,Xs,Ys,Zs).
+% Xs = [],
+% Ys = [1,2,1],
+% Zs = [3,2,1] ;
+% Xs = [1, 2],
+% Ys = [1, 3],
+% Zs = [2, 1] ;
+% false.
+separa(Ls,N,Xs,[Y|Ys],[Z|Zs]) :-
+	concatenar(Xs,YsZs,Ls), % genero Xs y las listas YsZs - condición 1
+	concatenar([Y|Ys],[Z|Zs],YsZs), % genero las listas [Y|Ys] y [Z|Zs], y de esta manera no están vacías - condición 1 y 2
+	suma(Ys,N). % compruebo la condición 3
