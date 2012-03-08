1765c6bb66e6e8ce931f1c4acce972695fcc2d08
Daniel Lopez <tauron32@gmail.com>
2012-03-08 18:41:34 +0100

    Ejercicio resuelto

--- TradCompInt/lex.yy.c
+++ TradCompInt/lex.yy.c
@@ -359,8 +359,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 8
+#define YY_END_OF_BUFFER 9
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -368,10 +368,11 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[18] =
+static yyconst flex_int16_t yy_accept[24] =
     {   0,
-        0,    0,    5,    4,    4,    4,    3,    0,    3,    3,
-        0,    0,    3,    2,    0,    1,    0
+        0,    0,    0,    0,    9,    8,    8,    8,    2,    4,
+        3,    2,    2,    1,    7,    7,    3,    3,    5,    7,
+        7,    6,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
@@ -408,35 +409,35 @@ static yyconst flex_int32_t yy_ec[256] =
 
 static yyconst flex_int32_t yy_meta[5] =
     {   0,
-        1,    2,    1,    3
+        1,    2,    3,    3
     } ;
 
-static yyconst flex_int16_t yy_base[22] =
+static yyconst flex_int16_t yy_base[28] =
     {   0,
-       20,   19,   22,   25,    0,    0,    0,    4,   19,    0,
-        0,   16,   17,   25,   16,    0,   25,    8,   10,   11,
-       14
+       21,   20,    0,   19,   22,   26,    2,   17,   26,   26,
+        6,    0,    0,   26,    0,   18,    0,    0,   26,    0,
+       13,   26,   26,   10,   13,   16,    7
     } ;
 
-static yyconst flex_int16_t yy_def[22] =
+static yyconst flex_int16_t yy_def[28] =
     {   0,
-       18,   18,   17,   17,   19,   17,   20,   17,   21,   20,
-        8,    8,   21,   17,    8,    8,    0,   17,   17,   17,
-       17
+       24,   24,   23,    3,   23,   23,   23,    7,   23,   23,
+       23,    7,    8,   23,   25,   26,   27,   11,   23,   25,
+       26,   23,    0,   23,   23,   23,   23
     } ;
 
-static yyconst flex_int16_t yy_nxt[30] =
+static yyconst flex_int16_t yy_nxt[31] =
     {   0,
-       17,   17,    8,    9,   11,   11,   12,   11,    4,    4,
-        4,   10,    7,   10,   13,   13,   13,   16,   14,   15,
-       14,   17,    6,    5,    3,   17,   17,   17,   17
+        9,   10,   11,   12,   14,   15,   17,   17,   18,   19,
+        6,    6,    6,   20,   22,   20,   21,   21,   21,   22,
+       16,   23,   13,    8,    7,    5,   23,   23,   23,   23
     } ;
 
-static yyconst flex_int16_t yy_chk[30] =
+static yyconst flex_int16_t yy_chk[31] =
     {   0,
-        0,    0,    6,    6,    8,    8,    8,    8,   18,   18,
-       18,   20,   19,   20,   21,   21,   21,   15,   13,   12,
-        9,    3,    2,    1,   17,   17,   17,   17,   17
+        3,    3,    3,    3,    7,    7,   11,   27,   11,   11,
+       24,   24,   24,   25,   21,   25,   26,   26,   26,   16,
+        8,    5,    4,    2,    1,   23,   23,   23,   23,   23
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -454,9 +455,11 @@ int yy_flex_debug = 0;
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
 #line 1 "Ejercicio1.l"
-#line 458 "lex.yy.c"
+
+#line 460 "lex.yy.c"
 
 #define INITIAL 0
+#define comentario 1
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -639,9 +642,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 1 "Ejercicio1.l"
+#line 2 "Ejercicio1.l"
 
-#line 645 "lex.yy.c"
+#line 648 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -695,13 +698,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 18 )
+				if ( yy_current_state >= 24 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 25 );
+		while ( yy_base[yy_current_state] != 26 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -726,29 +729,50 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 2 "Ejercicio1.l"
-{} /* Elimina un comentario tipo de bloque que ocupa toda la línea (o varias líneas) */
+#line 3 "Ejercicio1.l"
+{BEGIN(comentario);}
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
 #line 4 "Ejercicio1.l"
-{} /* Elimina la línea completa del comentario tipo línea */
+{ /* se come cualquier cosa que no sea un '*' */ }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
 #line 5 "Ejercicio1.l"
-{} /* Elimina el comentario tipo línea que no ocupa toda la línea */
+{ /* se come todos los '*' que no estén seguidos de '/' */ }
 	YY_BREAK
 case 4:
+/* rule 4 can match eol */
 YY_RULE_SETUP
 #line 6 "Ejercicio1.l"
+{ /* se come los saltos de línea internos */ }
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 7 "Ejercicio1.l"
+{BEGIN(INITIAL);} /* Detecta el fin del comentario */
+	YY_BREAK
+case 6:
+/* rule 6 can match eol */
+YY_RULE_SETUP
+#line 9 "Ejercicio1.l"
+{ /* Elimina la línea completa del comentario tipo línea */ }
+	YY_BREAK
+case 7:
+YY_RULE_SETUP
+#line 10 "Ejercicio1.l"
+{ /* Elimina el comentario tipo línea que no ocupa toda la línea */ }
+	YY_BREAK
+case 8:
+YY_RULE_SETUP
+#line 11 "Ejercicio1.l"
 ECHO;
 	YY_BREAK
-#line 751 "lex.yy.c"
+#line 774 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
+case YY_STATE_EOF(comentario):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1040,7 +1064,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 18 )
+			if ( yy_current_state >= 24 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1068,11 +1092,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 18 )
+		if ( yy_current_state >= 24 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 17);
+	yy_is_jam = (yy_current_state == 23);
 
 	return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1747,6 +1771,6 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 6 "Ejercicio1.l"
+#line 11 "Ejercicio1.l"
 
 
