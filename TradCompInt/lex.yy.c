2f21aeb42cd23b8343a9eb43d639885b587254b5
Daniel Lopez <tauron32@gmail.com>
2012-03-17 19:57:39 +0100

    Ejercicio 2 de la relación

--- TradCompInt/lex.yy.c
+++ TradCompInt/lex.yy.c
@@ -359,8 +359,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 2
-#define YY_END_OF_BUFFER 3
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -368,17 +368,17 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[8] =
+static yyconst flex_int16_t yy_accept[9] =
     {   0,
-        0,    0,    3,    2,    2,    1,    0
+        2,    2,    5,    2,    3,    1,    2,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -405,29 +405,29 @@ static yyconst flex_int32_t yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst flex_int32_t yy_meta[3] =
+static yyconst flex_int32_t yy_meta[4] =
     {   0,
-        1,    2
+        1,    2,    2
     } ;
 
-static yyconst flex_int16_t yy_base[9] =
+static yyconst flex_int16_t yy_base[10] =
     {   0,
-        0,    0,    4,    0,    5,    5,    5,    1
+        0,    0,    5,    0,    6,    6,    0,    6,    3
     } ;
 
-static yyconst flex_int16_t yy_def[9] =
+static yyconst flex_int16_t yy_def[10] =
     {   0,
-        7,    1,    7,    8,    7,    7,    0,    7
+        8,    1,    8,    9,    8,    8,    9,    0,    8
     } ;
 
-static yyconst flex_int16_t yy_nxt[8] =
+static yyconst flex_int16_t yy_nxt[10] =
     {   0,
-        4,    5,    6,    7,    3,    7,    7
+        4,    5,    6,    7,    8,    3,    8,    8,    8
     } ;
 
-static yyconst flex_int16_t yy_chk[8] =
+static yyconst flex_int16_t yy_chk[10] =
     {   0,
-        1,    1,    8,    3,    7,    7,    7
+        1,    1,    1,    9,    3,    8,    8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -444,8 +444,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "Ej1.l"
-#line 449 "lex.yy.c"
+#line 1 "Ej2.l"
+#line 2 "Ej2.l"
+	int caracteres = 0;
+	int palabras = 0;
+	int lineas = 0;
+#line 453 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -627,9 +631,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 2 "Ej1.l"
+#line 6 "Ej2.l"
 
-#line 633 "lex.yy.c"
+#line 637 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -682,13 +686,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 5 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -714,21 +718,26 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 /* rule 1 can match eol */
-*yy_cp = (yy_hold_char); /* undo effects of setting up yytext */
-(yy_c_buf_p) = yy_cp = yy_bp + 1;
-YY_DO_BEFORE_ACTION; /* set up yytext again */
 YY_RULE_SETUP
-#line 3 "Ej1.l"
-{
-			printf("%cCR",yytext[0]);
-		}
+#line 7 "Ej2.l"
+{lineas++; printf("Linea!\n");}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 6 "Ej1.l"
+#line 9 "Ej2.l"
+{caracteres += yyleng; palabras++;printf("Palabra!\n");}
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 10 "Ej2.l"
+{}
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 11 "Ej2.l"
 ECHO;
 	YY_BREAK
-#line 732 "lex.yy.c"
+#line 741 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1020,7 +1029,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1048,11 +1057,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 8);
 
 	return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1726,6 +1735,17 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 6 "Ej1.l"
+#line 11 "Ej2.l"
 
 
+int main() {
+	yylex();
+	
+	if (caracteres > 0) lineas++;
+	
+	printf("Numero de caracteres: %d\n",caracteres);
+	printf("Numero de palabras: %d\n",palabras);
+	printf("Numero de lineas: %d\n",lineas);
+	
+	return 0;
+}
